1ed06c0e7588622731459c0405e2ec8edc56fa8f
Volha Kireyeva <73903596+volhakireyeva@users.noreply.github.com>
2024-10-06 02:16:01 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://7iktxhm9a7.execute-api.us-east-1.amazonaws.com',
     order: 'http://localhost:3000/orders',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: true,
     import: false,
     bff: false,
